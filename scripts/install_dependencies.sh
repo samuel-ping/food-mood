1d8f3ced2104e6daee759fc96cce63885896c9e6
Sam Ping <samuelping42@gmail.com>
2020-05-11 20:54:21 -0400

    updated scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 npm install -y /var/www/food-mood
 npm install -y /var/www/food-mood/client
-npm run build /var/www/food-mood/client
+npm install -y /var/www/food-mood/client # for some reason, npm didn't add node_modules directory once so I put the command here twice just in case
---
+++ scripts/run_build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+npm run build /var/www/food-mood/client
+npm run build /var/www/food-mood/client # npm didn't run build once for some reason so I put the command here twice just in case
